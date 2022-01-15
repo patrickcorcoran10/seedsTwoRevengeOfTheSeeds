61e004871e61f9e0ca0ca123bfce12d6c101e13f
patrickcorcoran10 <patrickcorcoran10@gmail.com>
2022-01-15 12:58:02 -0600

    second deploy

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS crowdfund_db;
+CREATE DATABASE crowdfund_db;
+
+-- mysqldump -uroot -p$Password1 change_me_now_development > ~/change_me_now_development.sql
+-- mysql -uxrcswz1c5yn7rg1q -ps35vc2on6nqknqp2 kiepjcrlr5wu4cy2 --host=exbodcemtop76rnz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com > lib/tasks/data/db_dumps/pcdb_development_2022_01_15.sql
